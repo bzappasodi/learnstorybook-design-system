9aa49d1fb84337e3d5546b99e92629264f23b071
Bill Zappasodi <billyzapp@gmail.com>
2022-04-12 13:26:50 -0400

    fix tokens

--- src/Badge.stories.js
+++ src/Badge.stories.js
@@ -28,7 +28,7 @@ AllBadges.args = {
 AllBadges.storyName = "all badges";
 
 export const Positive = () => (
-  <Badge status="positive">hello chrissy rossi Positive</Badge>
+  <Badge status="positive">hello chrissy rossi you babe</Badge>
 );
 export const Negative = () => <Badge status="negative">Negative</Badge>;
 export const Warning = () => <Badge status="warning">Warning</Badge>;
